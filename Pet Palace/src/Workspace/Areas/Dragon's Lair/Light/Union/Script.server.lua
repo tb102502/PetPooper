ec72e3863f20ad716dc21096bd65999f26fb3090
tb102502 <bowlin.tommy3@gmail.com>
2025-05-21 07:44:51 -0400

    delete

--- Pet Palace/src/Workspace/Areas/Dragon's Lair/Light/Union/Script.server.lua
+++
@@ -1,8 +0,0 @@
-while true do
-	wait(math.random(2, 8))
-	script.Parent.Material = Enum.Material.Neon
-	script.Parent.PointLight.Enabled = true
-	wait(math.random(2, 8))
-	script.Parent.Material = Enum.Material.Glass
-	script.Parent.PointLight.Enabled = false
-end
