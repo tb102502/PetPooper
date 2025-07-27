8a229e5fdca349e6b3c4893ef71e6a266444f8de
tb102502 <bowlin.tommy3@gmail.com>
2025-07-27 17:14:09 -0400

    zxvz

--- Pet Palace/src/Workspace/𝐖𝐚𝐭𝐞𝐫𝐟𝐚𝐥𝐥 + 𝐑𝐢𝐯𝐞𝐫/River Rock Groups (Left)/RiverRockGroups.Location.server.lua
+++
@@ -1,1 +0,0 @@
-RiverRockGroups.Location: -484.477, 5.807, -5.562
--- Pet Palace/src/Workspace/𝐖𝐚𝐭𝐞𝐫𝐟𝐚𝐥𝐥 + 𝐑𝐢𝐯𝐞𝐫/Waterfall Water/Water Block/Script.server.lua
+++
@@ -1,3 +0,0 @@
-game.Workspace.Terrain:FillBlock(script.Parent.CFrame, script.Parent.Size, Enum.Material.Water)
-wait(1)
-script.Parent:remove()
